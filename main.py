0738468bbb7cc50b7d04a08d4cc80f83dbdd5308
nadyaklishena-collab <nadyaklishena@gmail.com>
2026-02-15 17:38:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def init_sheet():
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive",
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("CREDS_JSON", scope)
     client = gspread.authorize(creds)
     sheet = client.open_by_key(SPREADSHEET_ID).worksheet(WORKSHEET_NAME)
     return sheet
@@ -599,3 +599,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
